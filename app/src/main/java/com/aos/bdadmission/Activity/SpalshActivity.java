18611aaa9456011e2ffe8593a0b54be1823b4aa0
ar13101085 <ar13101085@gmail.com>
2017-08-20 18:44:24 +0600

    Signed-off-by: ar13101085 <ar13101085@gmail.com>

---
+++ app/src/main/java/com/aos/bdadmission/Activity/SpalshActivity.java
@@ -0,0 +1,25 @@
+package com.aos.bdadmission.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import bdadmission.R;
+
+public class SpalshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SpalshActivity.this,MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+
+}
